24ceebcac2d9f0f342e8bed2a56f9833fd6345ed
Yeseul Lee <dev.yezlee@gmail.com>
2021-11-17 14:51:13 +0000

    first commit

---
+++ js_101/main.js
@@ -0,0 +1,28 @@
+console.log("Hello, World");
+
+// task 1
+let myFavNumber = 80;
+
+if (myFavNumber === 5) {
+  console.log("My favourite number is 5 too!");
+}
+
+//task 2
+let answer = prompt("What's your name?");
+
+if (answer === "Ben") {
+  console.log("That's my name too!");
+} else {
+  console.log(answer + "?! That's a silly name...");
+}
+
+// task3
+let weaterType = "raining";
+
+if (weaterType === "raining") {
+  console.log("Take an umbrella!");
+} else if (weaterType === "sunny") {
+  console.log("Don't forget sunscream!");
+} else {
+  console.log("Must be a normal, grey, non-descript day!");
+}
